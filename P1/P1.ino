2f8572a115e2113a879bb923e0e2d7bced95a53e
Jonjon264 <148971024+Jonjon264@users.noreply.github.com>
2023-12-07 10:53:53 +0100

    Update P1.ino

--- P1/P1.ino
+++ P1/P1.ino
@@ -1,21 +1,114 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 
+Zumo32U4Motors motors;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4OLED display;
+Zumo32U4Encoders encoders;
+Zumo32U4IMU imu;
+Zumo32U4Buzzer buzzer;
+Zumo32U4ButtonA buttonA;
+
 float boundsX;
 float boundsY;
 int state;
 float lastPosition[3];
-float currentPosition[3];
 
 
+struct encoderData {
+  float distance[2];
+  float velocity[2];
+  long passedTime[2];
+  float passedDistance[2];
 
+  void getDistance() {
+    distance[0] = 8 * acos(0.0) * encoders.getCountsLeft() / 900;
+    distance[1] = 8 * acos(0.0) * encoders.getCountsRight() / 900;
+  }
+
+  void getVelocity() {
+    getDistance();
+
+    long dt = micros() - passedTime[0];
+    passedTime[0] = micros();
+    velocity[0] = 1000000 * (distance[0] - passedDistance[0]) / dt;
+    passedDistance[0] = distance[0];
+
+    dt = micros() - passedTime[1];
+    passedTime[1] = micros();
+    velocity[1] = 1000000 * (distance[1] - passedDistance[1]) / dt;
+    passedDistance[1] = distance[1];
+  }
+} encoderData;
+
+
+struct kinematics {
+  float currentPosition[3] = { 0, 0, 0 };
+  int r = 2;
+  int s = 85;
+  long excecutedTime = micros();
+
+  void forwardKinematics(float v1, float v2, float Offset) {
+    encoderData.getVelocity();
+    long dt = micros() - excecutedTime;
+    excecutedTime = micros();
+    currentPosition[0] += Offset * (v1 + v2) * cos(currentPosition[2] * PI / 180) * dt / (2 * 1000000);  //funktion (9)
+    currentPosition[1] += Offset * (v1 + v2) * sin(currentPosition[2] * PI / 180) * dt / (2 * 1000000);
+    currentPosition[2] += Offset * 50 * (v1 - v2) * dt / (s / 10 * 1000000);
+    if (currentPosition[2] < 0) {
+      currentPosition[2] = currentPosition[2] + 360;
+    }
+    if (currentPosition[2] > 360) {
+      currentPosition[2] = currentPosition[2] - 360;
+    }
+  }
+} kinematics;
+
+
+void patrol() {
+  if (treeDetected()) {
+    stop();
+    lastPosition[0] = kinematics.currentPosition[0];
+    state++;
+  } else {
+    movement();
+  }
+}
+
+void removeTree() {
+  //turn to tree
+  //move to tree
+  //push tree out of bounds
+}
+
+void getBack() {
+  //turn towards last position
+  //drive to last postiion
+  //turn to last postion
+  state = 0;
+}
+
+void movement() {
+}
+
+bool treeDetected() {
+  if (true) {
+    return true;
+  }
+}
+
+void stop(){
+  motors.setSpeeds(0,0);
+}
 
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
+  kinematics.forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
+  Serial.println((String) "X:\t" + kinematics.currentPosition[0] + "cm\t\tY:\t" + kinematics.currentPosition[1] + "cm\t\tθ:\t" + kinematics.currentPosition[2] + "°");
+  /*
   switch (state){
     case 0: 
       patrol();
@@ -27,39 +120,5 @@ void loop() {
       getBack();
       break;
   }
-}
-
-
-void patrol(){
-  if (treeDetected()){
-    stop();
-    lastPosition[0,1,2] = currentPosition[0,1,2]
-    state++;
-  }
-  else{
-      movement();
-  }
-}
-
-void removeTree(){
-  //turn to tree
-  //move to tree
-  //push tree out of bounds
-}
-
-void getBack(){
-  //turn towards last position
-  //drive to last postiion
-  //turn to last postion
-  state = 0;
-}
-
-void movement(){
-
-}
-
-bool treeDetected(){
-  if (true){
-    return true;
-  }
+  */
 }
