9244f0ea1166889c8e695c4d0191a8d7d42c0fc1
Jonjon264 <148971024+Jonjon264@users.noreply.github.com>
2023-12-08 09:29:09 +0100

    Update P1.ino

--- P1/P1.ino
+++ P1/P1.ino
@@ -9,6 +9,12 @@ Zumo32U4IMU imu;
 Zumo32U4Buzzer buzzer;
 Zumo32U4ButtonA buttonA;
 
+// variables for gyro
+uint32_t turnAngle = 0;
+int16_t turnRate;
+int16_t gyroOffset;
+uint16_t gyroLastUpdate = 0;
+
 float boundsX;
 float boundsY;
 int state;
@@ -20,6 +26,90 @@ void screen(float line1, float line2) {
   display.gotoXY(0, 1);
   display.print(line2);
 }
+
+void stop() {
+  motors.setSpeeds(0, 0);
+}
+
+/* 
+Gyro setup and convenience functions '
+*/
+void turnSensorSetup(){
+  Wire.begin();
+  imu.init();
+  imu.enableDefault();
+  imu.configureForTurnSensing();
+
+  display.clear();
+  display.print(F("Gyro cal"));
+
+  // Turn on the yellow LED in case the LCD is not available.
+  ledYellow(1);
+
+  // Delay to give the user time to remove their finger.
+  delay(500);
+
+  // Calibrate the gyro.
+  int32_t total = 0;
+  for (uint16_t i = 0; i < 1024; i++)
+  {
+    // Wait for new data to be available, then read it.
+    while(!imu.gyroDataReady()) {}
+    imu.readGyro();
+
+    // Add the Z axis reading to the total.
+    total += imu.g.z;
+  }
+  ledYellow(0);
+  gyroOffset = total / 1024;
+
+  // Display the angle (in degrees from -180 to 180) until the
+  // user presses A.
+  display.clear();
+  turnSensorReset();
+}
+
+// This should be called to set the starting point for measuring
+// a turn.  After calling this, turnAngle will be 0.
+void turnSensorReset(){
+  gyroLastUpdate = micros();
+  turnAngle = 0;
+}
+
+// Read the gyro and update the angle.  This should be called as
+// frequently as possible while using the gyro to do turns.
+void turnSensorUpdate(){
+  // Read the measurements from the gyro.
+  imu.readGyro();
+  turnRate = imu.g.z - gyroOffset;
+
+  // Figure out how much time has passed since the last update (dt)
+  uint16_t m = micros();
+  uint16_t dt = m - gyroLastUpdate;
+  gyroLastUpdate = m;
+
+  // Multiply dt by turnRate in order to get an estimation of how
+  // much the robot has turned since the last update.
+  // (angular change = angular velocity * time)
+  int32_t d = (int32_t)turnRate * dt;
+
+  // The units of d are gyro digits times microseconds.  We need
+  // to convert those to the units of turnAngle, where 2^29 units
+  // represents 45 degrees.  The conversion from gyro digits to
+  // degrees per second (dps) is determined by the sensitivity of
+  // the gyro: 0.07 degrees per second per digit.
+  //
+  // (0.07 dps/digit) * (1/1000000 s/us) * (2^29/45 unit/degree)
+  // = 14680064/17578125 unit/(digit*us)
+  turnAngle += (int64_t)d * 14680064 / 17578125;
+}
+
+uint32_t getTurnAngleInDegrees(){
+  turnSensorUpdate();
+  // do some math and pointer magic to turn angle in seconds to angle in degree
+  return (((uint32_t)turnAngle >> 16) * 360) >> 16;
+}
+
 
 struct encoderData {
   float distance[2];
@@ -61,7 +151,7 @@ struct kinematics {
     excecutedTime = micros();
     currentPosition[0] += Offset * (v1 + v2) * cos(currentPosition[2] * PI / 180) * dt / (2 * 1000000);  //funktion (9)
     currentPosition[1] += Offset * (v1 + v2) * sin(currentPosition[2] * PI / 180) * dt / (2 * 1000000);
-    currentPosition[2] += Offset * 50 * (v2 - v1) * dt / (s / 10 * 1000000);
+    currentPosition[2] = getTurnAngleInDegrees();
     if (currentPosition[2] < 0) {
       currentPosition[2] = currentPosition[2] + 360;
     }
@@ -83,8 +173,8 @@ struct kinematics {
     }
 
     if (turnAngle <= 180) {
-      while (turnAngle > 1) {
-        motors.setSpeeds(-100, 100);
+      while (turnAngle >= 1) {
+        motors.setSpeeds(-106, 100);
         forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
         turnAngle = teta - currentPosition[2];
         if (turnAngle < 0) {
@@ -96,8 +186,8 @@ struct kinematics {
     }
 
     else if (turnAngle > 180) {
-      while (turnAngle > 1) {
-        motors.setSpeeds(100, -100);
+      while (turnAngle >= 1) {
+        motors.setSpeeds(106, -100);
         forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
         turnAngle = teta - currentPosition[2];
         if (turnAngle < 0) {
@@ -105,7 +195,7 @@ struct kinematics {
         }
         screen(turnAngle, currentPosition[2]);
       }
-      motors.setSpeeds(0, 0);
+      stop();
     }
     screen(turnAngle, currentPosition[2]);
   }
@@ -115,20 +205,20 @@ struct kinematics {
 
     if (currentPosition[0] < x_d) {
       while (currentPosition[0] < x_d) {
-        motors.setSpeeds(100, 100);
+        motors.setSpeeds(106, 100);
         forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
         screen(x_d, currentPosition[0]);
       }
-      motors.setSpeeds(0, 0);
+      stop();
     }
 
     else if (currentPosition[0] > x_d) {
       while (currentPosition[0] > x_d) {
-        motors.setSpeeds(100, 100);
+        motors.setSpeeds(106, 100);
         forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
         screen(x_d, currentPosition[0]);
       }
-      motors.setSpeeds(0, 0);
+      stop();
     }
   }
 
@@ -139,8 +229,8 @@ struct kinematics {
     }
 
     if (turnAngle <= 180) {
-      while (turnAngle > 1) {
-        motors.setSpeeds(-100, 100);
+      while (turnAngle >= 1) {
+        motors.setSpeeds(-106, 100);
         forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
         turnAngle = angle_d - currentPosition[2];
         if (turnAngle < 0) {
@@ -148,12 +238,12 @@ struct kinematics {
         }
         screen(turnAngle, currentPosition[2]);
       }
-      motors.setSpeeds(0, 0);
+      stop();
     }
 
     else if (turnAngle > 180) {
-      while (turnAngle > 1) {
-        motors.setSpeeds(100, -100);
+      while (turnAngle >= 1) {
+        motors.setSpeeds(106, -100);
         forwardKinematics(encoderData.velocity[0], encoderData.velocity[1], 0.95);
         turnAngle = angle_d - currentPosition[2];
         if (turnAngle < 0) {
@@ -161,7 +251,7 @@ struct kinematics {
         }
         screen(turnAngle, currentPosition[2]);
       }
-      motors.setSpeeds(0, 0);
+      stop();
     }
     screen(turnAngle, currentPosition[2]);
   }
@@ -211,16 +301,15 @@ bool treeDetected() {
   }
 }
 
-void stop() {
-  motors.setSpeeds(0, 0);
-}
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  turnSensorSetup();
 }
 
 void loop() {
-  kinematics.backwardKinematics(10, 10, 0);
+  kinematics.backwardKinematics(-10, 10, 270);
 
   /*
   switch (state){
